fcc052810d70b41f9c1e3515f317ac006a87bca5
strxx <b1504550@planet.kanazawa-it.ac.jp>
2018-10-05 01:51:55 +0900

    4d

---
+++ src/ABC/kotlin/10/05/abc4b.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var f = readLine()!!
+    var s = readLine()!!
+    var t = readLine()!!
+    var l = readLine()!!
+    println(l.reversed())
+    println(t.reversed())
+    println(s.reversed())
+    println(f.reversed())
+}
+// reversedを使って並び替えるだけ
+// 90度とかだったら1111222233334444みたいに出力すればいいのかな
+// 270度なら4444333322221111
