7a52b81c7704925098809b031d4aef8c3097f191
strxx <b1504550@planet.kanazawa-it.ac.jp>
2018-10-14 00:33:56 +0900

    精進7日目

--- src/ABC/kotlin/2018Oct/abc09b.kt
+++ src/ABC/kotlin/2018Oct/abc09b.kt
@@ -1,2 +1,9 @@
-package ABC.kotlin.`2018Oct`
-
+fun main(args: Array<String>) {
+    var a = mutableSetOf<Int>()
+    for(i in 0 until readLine()!!.toInt()){
+        a.add(readLine()!!.toInt())
+    }
+    println(a.sortedDescending()[1])
+}
+// sortedDescending 逆順
+// 知らんけどsetだから重複を考えなくて良いから楽
