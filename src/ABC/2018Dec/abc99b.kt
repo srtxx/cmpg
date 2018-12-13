962eedec1b8ba7a3388e82cf364e24636bb2d683
strxx <b1504550@planet.kanazawa-it.ac.jp>
2018-12-14 00:40:08 +0900

    2018/12/14

--- src/ABC/2018Dec/abc99b.kt
+++ src/ABC/2018Dec/abc99b.kt
@@ -1,1 +1,17 @@
 fun main(args: Array<String>) {
+    var l = mutableListOf<Int>(1)
+    (2..999).map{i ->
+        l.add(l.last() + i)
+    }
+
+    val x = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
+    var i = 0
+    while(!l.containsAll(x)){
+        i += 1
+        x[0] += 1
+        x[1] += 1
+    }
+    println(i)
+}
+
+//よくわからないコーナーケースでた　普通に立式して解くらしい
